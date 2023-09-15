0e4a91d3852a9e39341f25713f4b8a701475a4c1
Graciela Caceres <141546396+gracielacaceres@users.noreply.github.com>
2023-09-15 09:30:31 -0500

    Add files via upload

---
+++ CalculadoraG/src/P1/Clase2.java
@@ -0,0 +1,16 @@
+
+package P1;
+import P2.Clase3;
+import P2.clase4;
+
+public class Clase2 {
+    public static void main(String[] args){
+        Clase1 objC1 = new Clase1();
+        Clase3 objC2 = new Clase3();
+        clase4 objC3 = new clase4();
+        
+        objC1.a = 1;
+        objC2.b = "texto";
+
+    }
+}
---
+++ CalculadoraG/src/P2/clase4.java
@@ -0,0 +1,12 @@
+
+package P2;
+
+/**
+ *
+ * @author Lab02
+ */
+public class clase4 {
+
+
+}
+
