970cdb2618bc03313eb7c4acba81675d64cd26bc
Graciela Caceres <141546396+gracielacaceres@users.noreply.github.com>
2023-10-03 10:00:37 -0500

    Add files via upload

---
+++ S07v2_07/Persona.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author graciela
+ */
+public class Persona {
+    // Atributos
+    String nombre;
+    int edad;
+
+    // Metodos
+    public void caminar() {
+        System.out.println("caminar");
+    }
+
+    public void correr() {
+        System.out.println("nombre");
+    }
+
+    //Sobre carga de metedos
+    public void saltar(){
+        
+    }
+    
+    public void saltar(int a){
+        
+    }
+    // Metodos con valores de retorno
+    public int nombre() {
+        return 0;
+    }
+
+ 
+    public int metodoInt(){
+        return 0;
+    }
+    
+    public  String texto(){
+        return "Hi";
+    }
+    
+    
+    public  double num(){
+        return 14.4;
+    }
+    
+    public void metodo1(int a){
+        
+    }
+    
+    public void metodo2(String a){
+        
+    }
+}
+
+
+
+
